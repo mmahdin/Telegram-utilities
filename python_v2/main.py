ad3db9614bebf3f32418c3a7e3c3180485a3cb42
mmahdin <mahdi9roozi@gmail.com>
2024-05-10 16:12:21 +0330

    Add functionality to search and download audio files based on size criteria from the latest outgoing chat message.

--- python_v2/main.py
+++ python_v2/main.py
@@ -1,5 +1,7 @@
 from telethon import TelegramClient, events
+from telethon.tl import types
 import asyncio
+import re
 
 
 # Define your API ID and Hash from Telegram
@@ -15,15 +17,53 @@ client = TelegramClient('anon', api_id, api_hash,
                         proxy=("http", '172.19.0.1', 8080))
 
 
-@client.on(events.NewMessage(incoming=True))
-async def auto_reply_handler(event):
-    if event.is_private:  # Check if the message is from a private chat
-        sender = await event.get_sender()
-        sender_id = sender.id
+async def search_audio_files_in_chat(chat_id, size):
+    audio_message_ids = []
+    async for message in client.iter_messages(chat_id):
+        if message.media:
+            try:
+                if isinstance(message.media, types.MessageMediaDocument):
+                    if message.media.document.mime_type.startswith('audio'):
+                        file_size = message.media.document.size
+                        print(message.media.document.mime_type, file_size)
+                        if file_size < size:
+                            audio_message_ids.append(message.id)
+            except AttributeError:
+                continue
+    return audio_message_ids
 
-        # Auto-reply to the sender
-        response_message = "Hello! I received your message."
-        await client.send_message(sender_id, response_message)
+
+async def download_audio_by_id(chat_id, message_id):
+    try:
+        message = await client.get_messages(entity=chat_id, ids=message_id)
+        if message and message.media:
+            await client.download_media(message, file=f'./media/{chat_id}_{message_id}.mp3')
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+
+@client.on(events.NewMessage(outgoing=True))
+async def log_outgoing_messages(event):
+    if event.is_private:
+        chat_id = event.chat_id
+        message_text = event.raw_text
+        await handle_search_and_download_audio(message_text, chat_id)
+
+
+async def handle_search_and_download_audio(message_text, chat_id):
+    if message_text.startswith("search4{") and message_text.endswith("}"):
+        print('pattern is ok')
+        extracted_text = message_text[8:-1]
+        match = re.match(r"(.+)\((\d+)\)", extracted_text)
+        if match:
+            print('pattern is mached')
+            data = match.group(1)
+            size = int(match.group(2))
+            audio_ids = await search_audio_files_in_chat(chat_id, size)
+            if audio_ids:
+                for audio_id in audio_ids:
+                    await download_audio_by_id(chat_id, audio_id)
+
 
 with client:
     client.start()
